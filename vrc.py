88e477389d50778f6c9532bd38b7e7256946df4e
mkcbe <48342699+mkcbe@users.noreply.github.com>
2019-03-08 10:30:16 +0200

    Update vrc.py

--- vrc.py
+++ vrc.py
@@ -1,12 +1,12 @@
 a=input()
 ascii=ord(a)
 if (65<=ascii)and(ascii<=90):
-    if(ascii==65)and(ascii==69)and(ascii==73)and(ascii==79)and(ascii==85):
+    if(a=='A')or(a=='E')or(a=='I')or(a=='O')or(a=='U'):
         print("Vowel")
     else:
         print("Consonant")
 elif(97<=ascii)and(ascii<=122):
-    if(ascii==97)and(ascii==101)and(ascii==105)and(ascii==111)and(ascii==117):
+    if(a=='A')or(a=='E')or(a=='I')or(a=='O')or(a=='U'):
         print("Vowel")
     else:
         print("Consonant")
