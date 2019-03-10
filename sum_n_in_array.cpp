bb413eba7a32669355e2ba5c0c114e70e13bc01e
mkcbe <48342699+mkcbe@users.noreply.github.com>
2019-03-10 10:03:21 +0530

    Update sum_n_in_array.cpp

--- sum_n_in_array.cpp
+++ sum_n_in_array.cpp
@@ -4,12 +4,15 @@ main()
 {
     int n,k,a[1000],sum;
     cin>>n>>k;
-    if(n>0&&k>=0)
+    if(n>0)
     {
         for(int i=0;i<n;i++)
             cin>>a[i];
-        for(int i=0;i<k;i++)
-            sum+=a[i];
-        cout<<sum;
+        if(k>0)
+        {
+            for(int i=0;i<k;i++)
+                sum+=a[i];
+            cout<<sum;
+        }
     }
 }
