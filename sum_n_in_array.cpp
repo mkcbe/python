b0ca41b4c46a29c8d9d2522f099e8eff0ac7a1c9
mkcbe <48342699+mkcbe@users.noreply.github.com>
2019-03-10 10:07:39 +0530

    Update sum_n_in_array.cpp

--- sum_n_in_array.cpp
+++ sum_n_in_array.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 main()
 {
-    int n,k,a[1000],sum;
+    int n,k,arry[10000],sum;
     cin>>n>>k;
     for(int i=0;i<n;i++)
-        cin>>a[i];
+        cin>>arry[i];
     for(int i=0;i<k;i++)
-        sum+=a[i];
+        sum+=arry[i];
     cout<<sum;
 }
