d0b8f397c979e3a1e178e6e0118eb43e0e4d7157
Mahesh Kanagaraj <48342699+mkcbe@users.noreply.github.com>
2019-04-15 07:51:14 +0530

    Update prime_number_between.py

--- prime_number_between.py
+++ prime_number_between.py
@@ -1,12 +1,10 @@
-lower,upper=input().split()
-lower,upper=int(lower),int(upper)
+lower,upper=map(int,input().split())
 prime=[]
-for num in range(lower,upper - 1):
+for num in range(lower+1,upper - 1):
    if num > 1:
        for i in range(2,num):
            if (num % i) == 0:
                break
        else:
-           #print(num)
            prime.append(num)
 print(*prime)
